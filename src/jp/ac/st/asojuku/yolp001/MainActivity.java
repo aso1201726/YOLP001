99ff50c4e0e5e7c4e83b66413ab981d218b820ce
student <student@PC111S19.fgakusei.local>
2014-07-28 15:03:08 +0900

    編集済み

--- src/jp/ac/st/asojuku/yolp001/MainActivity.java
+++ src/jp/ac/st/asojuku/yolp001/MainActivity.java
@@ -14,35 +14,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity implements LocationListener{
-	
+
 	LocationManager mLocationManager = null;
-	
+
 	MapView mMapView = null;
-	
+
 	int lastLatitude = 0;
-	
+
 	int lastLongitude = 0;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-	
-	
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		mMapView = new MapView(this, "c2VjcmV0Jng9Mjgdj0zaiZpPTdhZ1hERlB4QU01ViZzPWNvbnN1bWVyc2VjcmV0Jng9Mjg-");
-		
+		mMapView = new MapView(this, "dj0zaiZpPTdhZ1hERlB4QU01ViZzPWNvbnN1bWVyc2VjcmV0Jng9Mjg-");
+
 		mMapView.setBuiltInZoomControls(true);
-		
+
 		mMapView.setScalebar(true);
-		
+
 		//
 		//
-		double lat = 35.6585516;
+		double lat = 35.658516;
 		double lon = 139.701773;
 		GeoPoint gp = new GeoPoint((int)(lat * 1000000), (int)(lon * 1000000));
 		//
@@ -54,20 +53,59 @@ public class MainActivity extends Activity implements LocationListener{
 		//
 		setContentView(mMapView);
 		//
-		mLocationManager = 
-				(LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
 		Criteria criteria = new Criteria();
-		
+
 		criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-		
 		criteria.setPowerRequirement(Criteria.POWER_LOW);
+
 		String provider = mLocationManager.getBestProvider(criteria, true);
-		
+
 		mLocationManager.requestLocationUpdates(provider, 0, 0, this);
 	}
 
 
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		double lat = location.getLatitude();
+		int latitude = (int)(lat * 1000000);
+
+		double lon = location.getLongitude();
+		int longitude = (int)(lon * 1000000);
+
+		if(latitude/1000 != this.lastLatitude/1000 || longitude/1000 != this.lastLongitude/1000){
+			GeoPoint gp = new GeoPoint(latitude, longitude);
+
+			MapController c = mMapView.getMapController();
+
+			c.setCenter(gp);
+
+			this.lastLatitude = latitude;
+			this.lastLongitude = longitude;
+
+		}
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -75,59 +113,16 @@ public class MainActivity extends Activity implements LocationListener{
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	
 
-	@Override
-	public void onLocationChanged(Location location) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-		double lat = location.getLatitude();
-		int latitude = (int)(lat * 1000000);
-		
-		double lon = location.getLongitude();
-		int longitude = (int)(lon * 1000000);
-		
-		if(latitude/1000 != this.lastLatitude/1000 || longitude/1000 != this.lastLongitude/1000){
-			GeoPoint gp = new GeoPoint(latitude, longitude);
-			
-			MapController c = mMapView.getMapController();
-			
-			c.setCenter(gp);
-			
-			this.lastLatitude = latitude;			
-			this.lastLongitude = longitude;
-			
-		}
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+
 }
